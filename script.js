390ebaca22bdc4f99aebcbd9aac5833dae872e77
harbhlahritik <harbhlahritik@gmail.com>
2020-05-13 02:38:25 +0530

    Fixed and completed remaining speech flow

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const second = document.querySelector('.second');
 const third = document.querySelector('.third');
 const forth = document.querySelector('.forth');
 const fifth = document.querySelector('.fifth');
-const refresh = document.querySelector('.refresh');
+const confirm = document.querySelector('.confirm');
 
 second.style.display = 'none';
 third.style.display = 'none';
 forth.style.display = 'none';
 fifth.style.display = 'none';
-refresh.style.display = 'none';
+confirm.style.display = 'none';
 
 // voice functions and functionality
 
@@ -43,39 +43,157 @@ var getOption = false;
 
 recognition.onresult = (event) => {
     console.log(event);
-    const current = event.resultIndex;
+    var current = event.resultIndex;
     var transcript = event.results[current][0].transcript;
 
 
-    console.log("transcript: "+transcript);
-    console.log(typeof transcript);
+    if(getNumber) {
 
-    var number = transcript.replace(/\s+/g,'');
-    console.log("transcript formatted: "+number);
-    response.textContent = number;
-    electoralNumber = parseInt(number, 10);
-    console.log("electoralNumber: "+electoralNumber);
-    window.setTimeout(number_verification, 1000);
+        console.log('getNumber invoked');
+
+        electoralNumber = parseInt(transcript.replace(/\s+/g,''), 10);
+
+        getNumber = false;
+        getConfirmation = true;
+        
+        window.setTimeout(verification(electoralNumber), 1000);
+
+    } else if (getConfirmation) {
+
+        console.log("getConfirmation invoked");
+
+        if(transcript.includes('yes')){
+            
+            getConfirmation = false;
+            getOption = true;
+
+            window.setTimeout(getOptionfun, 1000);
+
+        }
+
+    } else if(getOption) {
+
+        console.log("getoption invoked");
+
+        getOption = false;
+        var option = 0;
+        
+        if(transcript.includes('1')){
+            option = 1;
+        } else if (transcript.includes('2')){
+            option = 2;
+        } else if (transcript.includes('3')){
+            option = 3;
+        } else {
+            option = 4;
+        }
+
+        window.setTimeout(optionVerification(option), 1000);
+
+
+    } else {
+
+        console.log("Confirm invoked");
+
+        window.setTimeout(confirm_vote, 1000);
+
+    }
 };
 
 
 // greetings voice
 
 const greetingText = "Welcome to Voice Automated Electoral Voting Machine EVM Demo. To begin the voting process you need to provide your Electoral voting number. Once ready press the below button";
-// speech(greetingText);
+speech(greetingText);
+
+
+
 
 // Electoral number verification
 
-function number_verification(electoralNumber) {
-    greetings.innerHTML = null;
+function verification(electoralNumber) {
+    console.log("verification function invoked");
 
-    const numberVerification = document.querySelector('.number-verification');
-    numberVerification.querySelector('p').innerHTML = electoralNumber;
+    second.style.display = 'block';
+
+    const electoralNumberDisplay = document.querySelector('.electoralNumberDisplay');
+    electoralNumberDisplay.textContent = electoralNumber;
+
+    var userDetails = {
+        "name": "New User",
+        "age": null,
+        "state": "Not Provided"
+    };
+
+    // get user data from electoral number
+    if(electoralNumber == 123123123){
+
+        userDetails.name = "Hritik Harbhla";
+        userDetails.age = 21;
+        userDetails.state = "Maharastra";
+
+    } else if (electoralNumber == 123412341234) {
+
+        userDetails.name = "Kundan Kumar";
+        userDetails.age = 22;
+        userDetails.state = "Delhi";
+
+    } else {
+        console.log("new User");
+    }
+
+    const name = document.querySelector(".name");
+    const age = document.querySelector(".age");
+    const state = document.querySelector(".state");
+    name.textContent = "Name: "+ userDetails.name;
+    age.textContent = "Age: "+ userDetails.age;
+    state.textContent = "State: "+ userDetails.state;
+
+    var text = "Your electoral number is "+ electoralNumber + ". Your name is " + userDetails.name + " with age " + userDetails.age + " and belonging to State " + userDetails.state;
+    speech(text);
+
+    text = "Do you wish to proceed furthur ?";
+    speech(text);
 }
 
 
 // option display
 
+function getOptionfun() {
+
+    forth.style.display = "block";
+
+    var text = "Please select one of the following options. Option 1 - Party 1, option 2 - party 2, option 3 - party 3, option 4 - nota. Say your option number to proceed.";
+    speech(text);
+
+}
+
 // option verification
 
+function optionVerification(option) {
+
+    fifth.style.display = "block";
+
+    const showSelectedOption = document.querySelector(".showSelectedOption");
+    showSelectedOption.textContent = "Option " + option;
+
+    var text = "You have selcted option number " + option;
+    speech(text);
+
+    text = "Say yes to confirm and cast the vote";
+    speech(text);
+
+}
+
 // Completion confirmation
+
+function confirm_vote() {
+
+    confirm.style.display = "block";
+
+    var text = "Thank you for casting your vote. Press the button below for another demonstration.";
+    speech(text);
+
+    button.style.display = "none";
+
+}
